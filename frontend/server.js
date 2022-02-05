5099793f0e6030188701cf619ac4ab5179786178
sjoongh <az45687@naver.com>
2022-02-05 23:14:38 +0900

    test

--- frontend/server.js
+++ frontend/server.js
@@ -68,12 +68,27 @@ app.post("/create", (req, res) => {
     // send file
 })
 // update board
-app.post("/create", (req, res) => {
+app.post("/update", (req, res) => {
     // 기존 data fetch 후 update
 
 })
 // delete board
-app.post("/create", (req, res) => {
+app.post("/delete", (req, res) => {
+    const result = {};
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const json = JSON.parse(data);
+
+        // 삭제 대상 없음
+
+        // DELETE JSON File
+        fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', err => {
+            result.success = true;
+            res.json(result);
+        })
+    })
     // no로 게시물 찾은후 delete
 })
 
