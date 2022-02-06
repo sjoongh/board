aa4b1611529e98d148cfe66d10168665cbf604fb
sjoongh <az45687@naver.com>
2022-02-06 14:51:45 +0900

    ajax 설정

--- frontend/server.js
+++ frontend/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+const { json } = require('body-parser');
 
 const app = express();
 const dbPath = path.join(__dirname, '/data/data.json');
@@ -14,8 +15,20 @@ app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
 app.use(bodyParser.json()); // 4.16이전
 // app.use(express.json()) --> 4.16이후
 
+
+// ======================================================================
 // ROUTES
 
+// 홈화면 index.html표시 --> 비동기로 완성할 것
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("src", "index.html"));
+    // fs.readFile(dbPath, 'utf8', (err, data) => {
+    //     if (err) throw err;
+    //     console.log(data,'sibal'); // 얘를 Home에 뿌려줌
+    // })
+});
+
+// get user test
 let test = {};
 
 test = JSON.parse(
@@ -26,6 +39,77 @@ test = JSON.parse(
 app.get("/data.json", (req, res, next) => {
     res.json(test);
 })
+
+// get board data
+app.get('/board', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw alert('error 발생!');
+
+        const { boardata } = JSON.parse(data);
+        res.json(boardata);
+    })
+})
+
+// create board
+app.post("/create", (req, res) => {
+    // no, title, name, date
+    const result = {};
+    const { no, title, name, date } = req.body;
+    
+    // add data
+    json.board.push(req.body);
+
+    // json 파일에 추가
+    fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', err => {
+        if (err) throw err;
+
+        result.success=true;
+        res.json(result);
+    })
+})
+// update board
+app.post("/update", (req, res) => {
+    const result {};
+    const { title, name } = req.params;
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const json = JSON.parse(data);
+
+        // update data
+        fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', err => {
+            if (err) throw err;
+
+            result.success = true;
+            res.json(result);
+        })
+    })
+
+})
+// delete board
+app.post("/delete", (req, res) => {
+    const result = {};
+    const { no } = req.params;
+
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const json = JSON.parse(data);
+
+        // 삭제 대상 없음
+
+        // DELETE JSON File
+        fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', err => {
+            result.success = true;
+            res.json(result);
+        })
+    })
+    // no로 게시물 찾은후 delete
+})
+
+app.listen(3000, () => console.log('Server is running... http://localhost:3000'));
+
 
 // create
 // fetch("http://localhost:3000/create", {
@@ -51,63 +135,3 @@ app.get("/data.json", (req, res, next) => {
 // let item = fetch("http://localhost:3000/data.json")
 //     .then(response => response.json())
 //     .then(json => console.log(json.board[0].name));
-
-// 홈화면 index.html표시
-// 비동기로 완성 해야함..
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("src", "index.html"));
-    // fs.readFile(dbPath, 'utf8', (err, data) => {
-    //     if (err) throw err;
-    //     console.log(data,'sibal'); // 얘를 Home에 뿌려줌
-    // })
-});
-
-// create board
-app.post("/create", (req, res) => {
-    res.sendFile(path.resolve("src", "index.html"));
-    // send file
-})
-// update board
-app.post("/update", (req, res) => {
-    // 기존 data fetch 후 update
-
-})
-// delete board
-app.post("/delete", (req, res) => {
-    const result = {};
-
-    fs.readFile(dbPath, 'utf8', (err, data) => {
-        if (err) throw err;
-
-        const json = JSON.parse(data);
-
-        // 삭제 대상 없음
-
-        // DELETE JSON File
-        fs.writeFile(dbPath, JSON.stringify(json, null, 2), 'utf8', err => {
-            result.success = true;
-            res.json(result);
-        })
-    })
-    // no로 게시물 찾은후 delete
-})
-
-app.listen(3000, () => console.log('Server is running... http://localhost:3000'));
-
-
-// ----------------------------------------------------------
-// const express = require("express");
-// const path = require("path");
-
-// // express 사용
-// const app = express();
-
-// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
-
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve("frontend", "index.html"));
-// });
-
-// // port 생성 서버 실행
-// app.listen(process.env.PORT || 3000, () => console.log("Server running ...."));
-
--- frontend/src/js/ajax.js
+++ frontend/src/js/ajax.js
@@ -1,4 +1,5 @@
 export default class Ajax {
+    // GET END POINT
     static get(url) {
         return new Promise((resolve, reject) => {
             const req = new XMLHttpRequest();
@@ -13,7 +14,8 @@ export default class Ajax {
             }
         });
     };
-    
+        
+    // CREATE END POINT
     static post(url, data) {
         return new Promise((resolve, reject) => {
             const req = new XMLHttpRequest();
@@ -22,6 +24,39 @@ export default class Ajax {
             req.send(JSON.stringify(data));
 
             req.onreadystatechange = function() {
+                if (req.readyState === XMLHttpRequest.DONE) {
+                    if (req.status === 200) resolve(req.response);
+                    else reject(req.statusText);
+                }
+            }
+        })
+    }
+   
+    // UPDATE END POINT
+    static put(url, id, data) {
+        return new Promise((resolve, reject) => {
+            const req = new XMLHttpRequest();
+            req.open('PUT', `${url}/${id}`);
+            req.setRequestHeader('Content-type', 'application/json');
+            req.send(JSON.stringify(data));
+
+            req.onreadystatechange = function () {
+                if (req.readyState === XMLHttpRequest.DONE) {
+                    if (req.status === 200) resolve(req.response);
+                    else reject(req.statusText);
+                }
+            }
+        })
+    }
+
+    // DELETE END POINT
+    static delete(url, id) {
+        return new Promise((resolve, reject) => {
+            const req = new XMLHttpRequest();
+            res.open('DELETE', `${url}/${id}`);
+            res.send();
+
+            req.onreadystatechange = function () {
                 if (req.readyState === XMLHttpRequest.DONE) {
                     if (req.status === 200) resolve(req.response);
                     else reject(req.statusText);
